9394eec815f74de2395928a6a3486d488fa8031b
Remco Tolsma <info@remcotolsma.nl>
2012-08-02 14:43:03 +0200

    Initial release.

---
+++ ipad-redirect.php
@@ -0,0 +1,97 @@
+<?php
+/*
+Plugin Name: iPad redirect
+Plugin URI:	http://pronamic.eu/wp-plugins/ipad-redirect/
+Description: This plugin makes it easy to redirect iPad users to an alternative URL.
+
+Version: 0.1
+Requires at least: 3.0
+
+Author: Pronamic
+Author URI: http://pronamic.eu/
+
+Text Domain: ipad_redirect
+Domain Path: /languages/
+
+License: GPL
+*/
+
+function ipad_redirect() {
+	if( is_singular( ) ) {
+		global $post;
+
+		$user_agent = filter_input( INPUT_SERVER, 'HTTP_USER_AGENT', FILTER_SANITIZE_STRING );
+	
+		$is_ipad = strpos( $user_agent, 'iPad') !== false;
+	
+		if ( $is_ipad ) {
+			$ipad_redirect_url = get_post_meta( $post->ID, '_ipad_redirect_url', true );
+	
+			if ( ! empty( $ipad_redirect_url ) ) {
+				wp_redirect( $ipad_redirect_url, 303 );
+	
+				exit;
+			}
+		}
+	}
+}
+
+add_action( 'template_redirect', 'ipad_redirect', 1 );
+
+function ipad_redirect_meta_box( $post ) {
+	wp_nonce_field( plugin_basename( __FILE__ ), 'ipad_redirect_nonce' );
+
+	printf( 
+		'<label for="ipad_redirect_url">%s</label>' , 
+		__( 'URL', 'ipad_redirect' )
+	);
+
+	printf( 
+		'<input type="url" id="ipad_redirect_url" name="ipad_redirect_url" value="%s" size="25" />' ,
+		esc_attr( get_post_meta( $post->ID, '_ipad_redirect_url', true ) )
+	);
+}
+
+function ipad_redirect_add_meta_boxes() {
+    add_meta_box(
+    	'ipad_redirect' , 
+		__( 'iPad Redirect', 'ipad_redirect' ) , 
+        'ipad_redirect_meta_box' , 
+        'page' , 
+		'side'
+    );
+}
+
+add_action( 'add_meta_boxes', 'ipad_redirect_add_meta_boxes' );
+
+function ipad_redirect_save_post( $post_id ) {
+	// verify if this is an auto save routine. 
+	// If it is our form has not been submitted, so we dont want to do anything
+	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) 
+		return;
+
+	// verify this came from the our screen and with proper authorization,
+	// because save_post can be triggered at other times
+	if ( !wp_verify_nonce( $_POST['ipad_redirect_nonce'], plugin_basename( __FILE__ ) ) )
+		return;
+  
+	// Check permissions
+	if ( 'page' == $_POST['post_type'] ) {
+    	if ( !current_user_can( 'edit_page', $post_id ) )
+        	return;
+  	} else {
+		if ( !current_user_can( 'edit_post', $post_id ) )
+			return;
+	}
+
+	// OK, we're authenticated
+	$url = filter_input( INPUT_POST, 'ipad_redirect_url', FILTER_VALIDATE_URL );
+
+	if ( ! empty ( $url ) ) {
+		update_post_meta( $post_id, '_ipad_redirect_url', $url );
+	} else {
+		delete_post_meta( $post_id, '_ipad_redirect_url' );
+	}
+}
+
+add_action( 'save_post', 'ipad_redirect_save_post' );
---
+++ readme.txt
@@ -0,0 +1,60 @@
+=== iPad redirect ===
+Contributors: pronamic, remcotolsma 
+Tags: ipad, redirect
+Donate link: http://pronamic.eu/donate/?for=wp-plugin-ipad-redirect&source=wp-plugin-readme-txt
+Requires at least: 3.0
+Tested up to: 3.4.1
+Stable tag: 0.1
+
+This plugin makes it easy to redirect iPad users to an alternative URL.
+
+== Description ==
+
+
+
+== Installation ==
+
+Extract the zip file and just drop the contents in the wp-content/plugins/ directory of your 
+WordPress installation and then activate the Plugin from Plugins page.
+
+
+== Screenshots ==
+
+
+
+== Changelog ==
+
+= 0.1 =
+*	Initial release
+
+
+== Pronamic ==
+*	[Pronamic](http://pronamic.eu/)
+*	[Twitter](http://twitter.com/pronamic)
+*	[Facebook](http://www.facebook.com/Pronamic)
+*	[LinkedIN](http://www.linkedin.com/company/pronamic)
+
+
+== Links ==
+
+*	[google-maps-utility-library-v3](http://code.google.com/p/google-maps-utility-library-v3/)
+*	[Geocode with Google Maps API v3](http://tech.cibul.org/geocode-with-google-maps-api-v3/)
+*	[WordPress event locations](http://icalevents.anmari.com/2343-event-locations-geo-tags-for-custom-post-types/)
+*	[JavaScript Regular Expression](http://lawrence.ecorp.net/inet/samples/regexp-format.php)
+*	[Remco Tolsma](http://remcotolsma.nl/)
+*	[Markdown's Syntax Documentation][markdown syntax]
+
+[markdown syntax]: http://daringfireball.net/projects/markdown/syntax
+            "Markdown is what the parser uses to process much of the readme file"
+
+
+== Pronamic plugins ==
+
+*	[Pronamic Google Maps](http://wordpress.org/extend/plugins/pronamic-google-maps/)
+*	[Gravity Forms (nl)](http://wordpress.org/extend/plugins/gravityforms-nl/)
+*	[Pronamic Page Widget](http://wordpress.org/extend/plugins/pronamic-page-widget/)
+*	[Pronamic Page Teasers](http://wordpress.org/extend/plugins/pronamic-page-teasers/)
+*	[Maildit](http://wordpress.org/extend/plugins/maildit/)
+*	[Pronamic Framework](http://wordpress.org/extend/plugins/pronamic-framework/)
+*	[Pronamic iDEAL](http://wordpress.org/extend/plugins/pronamic-ideal/)
+
